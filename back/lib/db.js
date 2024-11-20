1e0d97b0e3a56de1b9ce772cfb7a6eacdbf17bea
Peter Kiruhi <peterkiruhi4@gmail.com>
2024-11-20 15:56:44 +0300

    database connection

---
+++ back/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongo db connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error("Error connecting to database", error)
+        process.exit(1)
+    }
+}
