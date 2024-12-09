177fb9784a6acaff22a854eafe6884bf1637644d
Peter Kiruhi <peterkiruhi4@gmail.com>
2024-12-09 14:26:55 +0300

    updated the production backend url

--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "https://pintech-enterprises-tic6.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
