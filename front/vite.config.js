cc2051af7bf0f5ad9a6f401ec91cc3bfb16456f5
Peter Kiruhi <peterkiruhi4@gmail.com>
2024-12-13 15:07:11 +0300

    Proxy uri update

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-      proxy: { 
-        '/api': { 
-          target: 'http://localhost:5000', // Backend server URL 
-          changeOrigin: true,
-          secure: false, 
-        }, 
-      }, 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://pintech-enterprises-tic6.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
     },
-})
+  },
+});
