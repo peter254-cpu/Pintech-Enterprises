9c507f408bc808c075e967c377a4bbe6c90aeece
Peter Kiruhi <peterkiruhi4@gmail.com>
2024-12-20 12:35:28 +0300

    changed the base folder

--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "https://pintech-enterprises-5vvi.onrender.com/api",
+	baseURL: "h/api",
 	withCredentials: true, // send cookies to the server
 });
 
